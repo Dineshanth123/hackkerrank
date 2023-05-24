8bb6d2a04491a96ff98345ec783fba1fcba952eb
Dineshanth123 <123295107+Dineshanth123@users.noreply.github.com>
2023-05-24 09:42:24 +0530

    Add files via upload

---
+++ travelling concession.py
@@ -0,0 +1,7 @@
+y=int(input("enter your year of birth"))
+z=2023-y
+if(z>=60):
+      a=1020-((20*1020)//100)
+      print("your travelling fee is",a)
+else:
+    print("your travelling fee is 1020")
