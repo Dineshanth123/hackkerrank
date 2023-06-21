4a4f57dee02fb4c2d1d1eb4e2114e97e0a984bcc
Dineshanth123 <123295107+Dineshanth123@users.noreply.github.com>
2023-06-21 21:37:24 +0530

    Add files via upload

---
+++ Simpsonthreeeight.py
@@ -0,0 +1,33 @@
+from math import *
+l=int(input("enter the lower limit :"))
+u=int(input("enter the upper limit :"))
+n=int(input("enter number of terms :"))
+fun=input("enter function :")
+h=(u-l)//n
+print("the value of h is :",h)
+l=[]
+def f(x):
+    t=eval(fun)
+    return round(t,4)
+for i in range(u+1):
+    s=f(i)
+    l.append(s)
+x=l[0]+l[-1]
+a=len(l)-1
+l1=[]
+for p in range(1,a):
+    if p%3==0:
+        continue
+    l1.append(l[p])
+add=sum(l1)
+y=round(add,4)
+l2=[]
+for p in range(1,a):
+    if p%3!=0:
+        continue
+    l2.append(l[p])
+add1=sum(l2)
+y=round(add1,4)
+res=((3*h)/8)*(x+(3*(add))+(2*(add1)))
+print("the result is :",res)
+   
