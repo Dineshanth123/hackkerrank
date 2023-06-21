e315f2f63e30bbaabc976de98eca7b06161d1f79
Dineshanth123 <123295107+Dineshanth123@users.noreply.github.com>
2023-06-21 09:16:13 +0530

    Add files via upload

---
+++ Eulers.py
@@ -0,0 +1,16 @@
+from math import *
+x=float(input("enter x0 :"))
+y=float(input("enter y0 :"))
+h=float(input("enter h :"))
+fun=input("enter function[NOTE:USE * FOR MULTIPLICATION,USE / FOR DIVISION] :")
+n1=float(input("enter value of x :"))
+n2=n1//h
+n=int(n2)
+print("Number of Iterations :",n)
+def f(x,y):
+    e=eval(fun)
+    return e
+for i in range(n):
+    y=y+h*f(x,y)
+    print("the value of y at {0} is {1}".format(round(x+h,4),round(y,4)))
+    x=x+h
